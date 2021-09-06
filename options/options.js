452b311cd9f68ebe0b2a767d8dbca106284c66c6
Crowdbotics <team@crowdbotics.com>
2021-09-06 06:45:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "reduxdemo_30345Identifier",
+  url: "https://reduxdemo_30345Identifier.botics.co",
+  api: "https://reduxdemo_30345Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
